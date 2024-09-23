2934eaa643558bca29631a35c0d6a589519571f8
DmitriyGulevich <kaskodim@gmail.com>
2024-09-23 11:54:55 +0300

    стилизация секции contact me

--- src/assets/layout/sections/contacts/Contacts.tsx
+++ src/assets/layout/sections/contacts/Contacts.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import {SectionTitle} from "../../../../coponents/SectionTitle";
 import contactImage from './../../../images/contact-image.png'
-
+import {Container} from "../../../../coponents/Container";
+import {ReactComponent as PaperPlane} from "../../../images/icon/iconPaperPlane.svg";
 
 export const Contacts = () => {
     return (
         <SContacts>
-            <SectionTitle>Contact me</SectionTitle>
-            <FormWrapper>
+            <Container>
+                <SectionTitle>Contact me</SectionTitle>
 
-                <img src={contactImage} alt="contact"/>
-                <SForm>
-                    <input type='email' placeholder='Enter email address'/>
-                    <textarea placeholder='Enter message...'/>
-                    <button type="submit">Send Message</button>
-                </SForm>
 
-            </FormWrapper>
+                <FormWrapper>
+
+                    <Image src={contactImage} alt="contact"/>
+                    <SForm>
+                        <FieldWrapper>
+                            <Input type='email' placeholder='Enter email address'/>
+                            <TextArea placeholder='Enter message...'/>
+                        </FieldWrapper>
+
+                        <Button type="submit">
+
+                            <span>Send Message</span>
+                            <PaperPlane/>
+                        </Button>
+                    </SForm>
+
+                </FormWrapper>
+            </Container>
+
 
         </SContacts>
     );
@@ -25,18 +38,75 @@ export const Contacts = () => {
 
 
 const SContacts = styled.section`
-    background-color: aqua;
+    margin-bottom: 70px;
+`
 
+const Image = styled.img`
+    height: 412px;
 `
 
 const FormWrapper = styled.div`
-display: flex;
-    justify-content: space-around;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 30px;
 `
 
 const SForm = styled.form`
-display: flex;
-    flex-direction: column;
-    min-width:312px;
+    display: block;
+    min-width: 312px;
     gap: 30px;
+    width: 100%;
+    margin: 30px 0 30px 65px;
+
+
+`
+
+const FieldWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-radius: 4px;
+
+
+`
+
+const fieldStyles = css`
+    padding: 12px 18px 12px 18px;
+    border: 1px solid rgba(0, 0, 0, 0.12);
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2),
+    0 2px 1px 0 rgba(0, 0, 0, 0.12),
+    0 1px 1px 0 rgba(0, 0, 0, 0.14);
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 28px;
+`
+
+const Input = styled.input`
+    height: 54px;
+    ${fieldStyles}
+`
+
+const TextArea = styled.textarea`
+    margin: 40px 0;
+    outline: 1px solid rgba(0, 0, 0, 0.12);
+    height: 158px;
+    resize: none;
+    ${fieldStyles}
+`
+
+const Button = styled.button`
+    height: 50px;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    padding: 12px 24px 12px 24px;
+    background: #343D68;
+    color: white;
+    margin: 0 auto;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 26px;
+
 `
--- src/coponents/button/button.ts
+++ src/coponents/button/button.ts
@@ -18,7 +18,7 @@ export const Button = styled.button`
     margin-top: auto;
 
     &:hover {
-        animation: gradientAnimation 2s ease-in-out;
+        animation: gradientAnimation 2s ease-in-out infinite;
     }
 
 
